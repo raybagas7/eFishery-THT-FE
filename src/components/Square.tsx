b6e11419b2fbcfe0065b2269acf0fe4c50ecdf1e
raybagas7 <raysam.gas@gmail.com>
2024-05-07 15:19:54 +0700

    feat(sass):
        - add sass dependencies
        - make variable and colors variation for some class with scss
        - make sample component called suare to test

---
+++ src/components/Square.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./square.module.scss";
+
+const Square = () => {
+  return (
+    <div className={`${style.text_black} ${style.bg_primary_light_3}`}>
+      Square asd
+    </div>
+  );
+};
+
+export default Square;
