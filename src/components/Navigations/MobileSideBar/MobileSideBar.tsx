c3d4711a583e9cf683879c0bfdf113e5214b0f46
raybagas7 <raysam.gas@gmail.com>
2024-05-08 11:12:09 +0700

    feat(mobile sidebar): animated sidebar
        - Use framer motion to show sidebar
        - Add eFishery banner to the sidebar

--- src/components/Navigations/MobileSideBar/MobileSideBar.tsx
+++ src/components/Navigations/MobileSideBar/MobileSideBar.tsx
@@ -1,8 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MobileNavigation from "../MobileNavigation/MobileNavigation";
+import style from "./MobileSideBar.module.scss";
+import { motion, useAnimationControls } from "framer-motion";
+import Image from "next/image";
 
 const MobileSideBar = () => {
   const [asideHide, setAsideHide] = useState<boolean | undefined>();
+
+  const containerVariants = {
+    close: {
+      opacity: 0,
+      transitionEnd: {
+        display: "none",
+      },
+    },
+    open: {
+      opacity: 1,
+      display: "block",
+    },
+  };
+
+  const sideBar = {
+    close: {
+      x: "-300%",
+    },
+    open: {
+      x: "0%",
+    },
+  };
+
+  const containerAnimationControls = useAnimationControls();
+
+  useEffect(() => {
+    if (asideHide) {
+      containerAnimationControls.start("open");
+    } else {
+      containerAnimationControls.start("close");
+    }
+  }, [asideHide]);
 
   const toggleAside = () => {
     setAsideHide((prev) => !prev);
@@ -11,7 +46,29 @@ const MobileSideBar = () => {
   return (
     <>
       <MobileNavigation toggleAside={toggleAside} />
-      {asideHide && <div>SHOW ASIDE</div>}
+      <motion.div
+        variants={containerVariants}
+        animate={containerAnimationControls}
+        initial="close"
+        onClick={toggleAside}
+        className={style.backdrop_container}
+      >
+        <motion.div
+          variants={sideBar}
+          transition={{ type: "just" }}
+          animate={containerAnimationControls}
+          initial="close"
+          onClick={(e) => e.stopPropagation()}
+          className={style.mobile_sidebar_container}
+        >
+          <Image
+            src={"/efishery.png"}
+            width={200}
+            height={50}
+            alt="efish_banner"
+          />
+        </motion.div>
+      </motion.div>
     </>
   );
 };
