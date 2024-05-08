501f902e45ad888db3a2ba0d0293c6ccd551dc76
raybagas7 <raysam.gas@gmail.com>
2024-05-08 16:08:19 +0700

    feat(add page): implement main layout to the page
        - implement MainLayout into add new commodity page

--- src/pages/commodity/add/index.tsx
+++ src/pages/commodity/add/index.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import MainLayout from "@/components/Layout/MainLayout";
+import React, { ReactElement } from "react";
 
-const index = () => {
-  return <div>index</div>;
+const AddCommodity = () => {
+  return <div>AddCommodity</div>;
 };
 
-export default index;
+export default AddCommodity;
+
+AddCommodity.getLayout = function getLayout(page: ReactElement) {
+  return <MainLayout>{page}</MainLayout>;
+};
