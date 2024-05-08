bc44b14b181c6f0d3b83a0c7f9cda4b755934fe3
raybagas7 <raysam.gas@gmail.com>
2024-05-08 15:50:18 +0700

    feat(top navigation): add link to the icon
        - wrap eFishery icon with link to go to home page

--- src/components/Navigations/MobileNavigation/MobileNavigation.tsx
+++ src/components/Navigations/MobileNavigation/MobileNavigation.tsx
@@ -3,17 +3,20 @@ import style from "./MobileNavigation.module.scss";
 import Image from "next/image";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { IMobileNavigation } from "@/interfaces/components";
+import Link from "next/link";
 
 const MobileNavigation = ({ toggleAside }: IMobileNavigation) => {
   return (
     <div className={style.mobile_nav_container}>
       <div className={style.box}>
-        <Image
-          src={"/icon-192x192.png"}
-          width={50}
-          height={50}
-          alt="eFishery Icon"
-        />
+        <Link href={"/"}>
+          <Image
+            src={"/icon-192x192.png"}
+            width={50}
+            height={50}
+            alt="eFishery Icon"
+          />
+        </Link>
         <GiHamburgerMenu onClick={toggleAside} className={style.hamburger} />
       </div>
     </div>
