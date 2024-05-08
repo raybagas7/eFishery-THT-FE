6e8ac38004619de33c60a9656b37d608e3cf4a23
raybagas7 <raysam.gas@gmail.com>
2024-05-08 18:06:43 +0700

    refactor(deps): calendar deps
        - uninstall react-day-picker date-fns react-tiny-popover
        - replace that with react-datepicker

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,11 +1,7 @@
-import "react-day-picker/src/style.css";
 import React from "react";
-import { useState } from "react";
-import { DayPicker } from "react-day-picker";
 
 const Calendar = () => {
-  const [selected, setSelected] = useState<Date>();
-  return <DayPicker mode="single" selected={selected} onSelect={setSelected} />;
+  return <div>asd</div>;
 };
 
 export default Calendar;
