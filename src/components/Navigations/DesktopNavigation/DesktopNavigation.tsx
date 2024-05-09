51f2e495f063a294865da677cc840e731bd423af
raybagas7 <raysam.gas@gmail.com>
2024-05-09 13:58:36 +0700

    feat(desktop navigation): desktop navigation style
        - use breakpoint to show and hide desktop navigation
        - put eFishery banner on top of aside navigation

--- src/components/Navigations/DesktopNavigation/DesktopNavigation.tsx
+++ src/components/Navigations/DesktopNavigation/DesktopNavigation.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import style from "./DesktopNavigation.module.scss";
 import MobileSideBar from "../MobileSideBar/MobileSideBar";
+import Image from "next/image";
 
 const DesktopNavigation = () => {
   return (
     <>
       <MobileSideBar />
-      <div className={style.desktop_nav_container}>DesktopNavigation</div>
+      <div className={style.desktop_nav_container}>
+        <div className={style.desktop_nav_box}>
+          <div className={style.banner_box}>
+            <Image
+              src={"/efishery.png"}
+              width={200}
+              height={50}
+              alt="efish_banner"
+            />
+          </div>
+          <nav className={style.navigation}>
+            <ul></ul>
+          </nav>
+        </div>
+      </div>
     </>
   );
 };
